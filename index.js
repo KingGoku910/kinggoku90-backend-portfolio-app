f276245fccb64eff578d02afe08be81255bec873
KingGoku901 <ryno.rossouw@outlook.com>
2022-11-13 12:23:18 +0200

    Finished up the URL Shortner and pushing to live platform

--- index.js
+++ index.js
@@ -3,27 +3,38 @@
 
 
 // init project
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var shortId = require('shortid');
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config()
+
+
+
+//Setup DB
+mongoose.connect(process.env.DB_URI)
 
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const shortid = require('shortid');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
@@ -40,10 +51,10 @@ app.get("/requestHeaderParser", (req, res) => {
     res.sendFile(__dirname + '/views/requestHeaderParser.html');
 });
 
-//Request Header parser router
+//URL Shortner
 
-app.get("/urlShortner", (req, res) => {
-    res.sendFile(__dirname + '/views/urlShortner.html');
+app.get("/urlShortenerMicroservice", (req, res) => {
+    res.sendFile(__dirname + '/views/urlShortenerMicroservice.html');
 });
 
 // Timestamp Project
@@ -96,15 +107,56 @@ app.get("/api/whoami", (req, res) => {
 });
 
 //URL Shortner
+//build schema & model to store urls
 
-app.get("/api/urlShortner", (req, res) => {
-    req.json({
-        
+var ShortURL = mongoose.model('ShortURL', new mongoose.Schema({
+    original_url: String,
+    suffix: String,
+    short_url: String
+}));
 
+//parse app/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+//Create app json parser 
+var jsonParser = bodyParser.json()
+
+app.post("/api/shorturl/new/", (req, res) => {
+    let requestedUrl = req.body.url
+    let suffix = shortid.generate();
+    let newShortUrl = suffix;
+
+    let newURL = new ShortURL({
+        original_url: requestedUrl,
+        suffix: suffix,
+        short_url: __dirname + "/api/shorturl/" + suffix
     })
-})
+
+    newURL.save((err, doc) => {
+        if (err) return console.log(err);
+        res.json({
+            "saved": true,
+            "original_url": newURL.original_url,
+            "suffix": newURL.suffix,
+            "short_url": newURL.short_url
+        });
+    });
+});
+
+app.get("/api/shorturl/:suffix", (req, res) => {
+    let userSuffix = req.params.suffix;
+    ShortURL.find({ suffix: userSuffix }).then((foundUrls) => {
+        let urlForRedirect = foundUrls[0];
+        res.redirect(urlForRedirect.original_url);
+        
+    });
+});
+
+
 
 // listen for requests :)
-var listener = app.listen(port, function () {
+var listener = app.listen(port, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
